adcebbb1ddba22f3c9090124eb243275ca2ed61b
MarcusGitAccount <pop_marcus_98@yahoo.com>
2019-12-29 23:43:15 +0200

    tabs to spaces conversion

--- PGSceneFinalProject/Terrain.cpp
+++ PGSceneFinalProject/Terrain.cpp
@@ -4,151 +4,151 @@
 
 namespace gps {
 
-	Terrain::Terrain(void) {
+  Terrain::Terrain(void) {
 
-	}
+  }
 
-	Terrain::Terrain(int verticesCount) {
-		this->verticesCount = verticesCount;
-		this->wasGenerated = false;
-		this->terrainMesh = this->generateTerrain();
-	}
+  Terrain::Terrain(int verticesCount) {
+    this->verticesCount = verticesCount;
+    this->wasGenerated = false;
+    this->terrainMesh = this->generateTerrain();
+  }
 
-	void Terrain::Draw(gps::Shader shader) {
-		if (!this->wasGenerated) {
-			fprintf(stderr, "Terrain can not be drawn because it was not generated.");
-			return;
-		}
+  void Terrain::Draw(gps::Shader shader) {
+    if (!this->wasGenerated) {
+      fprintf(stderr, "Terrain can not be drawn because it was not generated.");
+      return;
+    }
 
-		this->terrainMesh.Draw(shader);
-	}
+    this->terrainMesh.Draw(shader);
+  }
 
-	Mesh Terrain::generateTerrain() {
-		std::vector<gps::Vertex> vertices;
-		std::vector<GLuint> indices;
-		std::vector<gps::Texture> textures;
+  Mesh Terrain::generateTerrain() {
+    std::vector<gps::Vertex> vertices;
+    std::vector<GLuint> indices;
+    std::vector<gps::Texture> textures;
 
-		float vx, vy, vz;
+    float vx, vy, vz;
 
-		std::default_random_engine generator;
-		std::uniform_real_distribution<float> distribution(-.5, +.5);
-		auto rng = std::bind(distribution, generator);
+    std::default_random_engine generator;
+    std::uniform_real_distribution<float> distribution(-.5, +.5);
+    auto rng = std::bind(distribution, generator);
 
-		for (int i = 0; i < this->verticesCount; i++) {
-			for (int j = 0; j < this->verticesCount; j++) {
-				vx = 2 * (float)j / (float)(this->verticesCount - 1) - 1; // cover the whole plane: [0, 1] to [-1, 1]
-				vy = (getHeightAtCoord(j, i, 0) + 1.0f) * .5; // [-1, 1] to [0, 1]
-				vz = 2 * (float)i / (float)(this->verticesCount - 1) - 1;
+    for (int i = 0; i < this->verticesCount; i++) {
+      for (int j = 0; j < this->verticesCount; j++) {
+        vx = 2 * (float)j / (float)(this->verticesCount - 1) - 1; // cover the whole plane: [0, 1] to [-1, 1]
+        vy = (getHeightAtCoord(j, i, 0) + 1.0f) * .5; // [-1, 1] to [0, 1]
+        vz = 2 * (float)i / (float)(this->verticesCount - 1) - 1;
 
-				glm::vec3 vertexPosition(vx, vy, vz);
-				glm::vec3 vertexNormal = glm::vec3(0.0f, 0.0f, 0.0f); // default normal, will be overriden
-				glm::vec2 vertexTexCoords(0, 0);
+        glm::vec3 vertexPosition(vx, vy, vz);
+        glm::vec3 vertexNormal = glm::vec3(0.0f, 0.0f, 0.0f); // default normal, will be overriden
+        glm::vec2 vertexTexCoords(0, 0);
 
-				gps::Vertex currentVertex;
-				currentVertex.Position = vertexPosition;
-				currentVertex.Normal = vertexNormal;
-				currentVertex.TexCoords = vertexTexCoords;
+        gps::Vertex currentVertex;
+        currentVertex.Position = vertexPosition;
+        currentVertex.Normal = vertexNormal;
+        currentVertex.TexCoords = vertexTexCoords;
 
-				vertices.push_back(currentVertex);
-			}
-		}
-		
-		// Compute normals for each point using its 4 neighbours
-		/* 
-			OBSOLOTE, useless, redundant, unsatisfactory, unpleasing, faulty, buggy
+        vertices.push_back(currentVertex);
+      }
+    }
 
-			for (int i = 1; i < this->verticesCount - 1; i++) {
-				for (int j = 1; j < this->verticesCount - 1; j++) {
-					int index = i * this->verticesCount + j;
+    // Compute normals for each point using its 4 neighbours
+    /*
+      OBSOLOTE, useless, redundant, unsatisfactory, unpleasing, faulty, buggy
 
-					// Get east, west, south and north neighbours
-					float left  = vertices.at(index - 1).Position.z;
-					float right = vertices.at(index + 1).Position.z;
-					float up		= vertices.at((i + 1) * this->verticesCount + j).Position.z;
-					float down  = vertices.at((i - 1) * this->verticesCount + j).Position.z;
+      for (int i = 1; i < this->verticesCount - 1; i++) {
+        for (int j = 1; j < this->verticesCount - 1; j++) {
+          int index = i * this->verticesCount + j;
 
-					float nx = (left + right) / 2.0f;
-					float nz = (up + down) / 2.0f;
+          // Get east, west, south and north neighbours
+          float left  = vertices.at(index - 1).Position.z;
+          float right = vertices.at(index + 1).Position.z;
+          float up		= vertices.at((i + 1) * this->verticesCount + j).Position.z;
+          float down  = vertices.at((i - 1) * this->verticesCount + j).Position.z;
 
-					vertices.at(index).Normal = glm::vec3(nx, 1.0f, nz);
-				}
-			}
-		*/
+          float nx = (left + right) / 2.0f;
+          float nz = (up + down) / 2.0f;
 
-		// The normal of each vertex position is equal to
-		// normalize(sum(normal of triangle(i) adjiacent to our vertex))
-		
+          vertices.at(index).Normal = glm::vec3(nx, 1.0f, nz);
+        }
+      }
+    */
 
-		GLuint innerTrianglePairsCount = this->verticesCount - 1;
-		for (int i = 0; i < innerTrianglePairsCount; i++) {
-			for (int j = 0; j < innerTrianglePairsCount; j++) {
+    // The normal of each vertex position is equal to
+    // normalize(sum(normal of triangle(i) adjiacent to our vertex))
 
-				// Get the 4 points indices enclosing our pair of triangles
-				GLuint bottomLt = i * this->verticesCount + j;
-				GLuint bottomRt = bottomLt + 1;
-				GLuint topLt = (i + 1) * this->verticesCount + j;
-				GLuint topRt = topLt + 1;
 
-				// Get each of 4 vertices
-				glm::vec3 vecBottomLt = vertices.at(bottomLt).Position;
-				glm::vec3 vecBottomRt = vertices.at(bottomRt).Position;
-				glm::vec3 vecTopLt = vertices.at(topLt).Position;
-				glm::vec3 vecTopRt = vertices.at(topRt).Position;
+    GLuint innerTrianglePairsCount = this->verticesCount - 1;
+    for (int i = 0; i < innerTrianglePairsCount; i++) {
+      for (int j = 0; j < innerTrianglePairsCount; j++) {
 
-				// first triangle
-				glm::vec3 ab = vecBottomLt - vecTopLt;
-				glm::vec3 ac = vecBottomRt - vecTopLt;
-				glm::vec3 normal = glm::normalize(glm::cross(ab, ac));
+        // Get the 4 points indices enclosing our pair of triangles
+        GLuint bottomLt = i * this->verticesCount + j;
+        GLuint bottomRt = bottomLt + 1;
+        GLuint topLt = (i + 1) * this->verticesCount + j;
+        GLuint topRt = topLt + 1;
 
-				indices.push_back(topLt);
-				indices.push_back(bottomLt);
-				indices.push_back(bottomRt);
+        // Get each of 4 vertices
+        glm::vec3 vecBottomLt = vertices.at(bottomLt).Position;
+        glm::vec3 vecBottomRt = vertices.at(bottomRt).Position;
+        glm::vec3 vecTopLt = vertices.at(topLt).Position;
+        glm::vec3 vecTopRt = vertices.at(topRt).Position;
 
-				vertices.at(topLt).Normal += normal;
-				vertices.at(bottomLt).Normal += normal;
-				vertices.at(bottomRt).Normal += normal;
+        // first triangle
+        glm::vec3 ab = vecBottomLt - vecTopLt;
+        glm::vec3 ac = vecBottomRt - vecTopLt;
+        glm::vec3 normal = glm::normalize(glm::cross(ab, ac));
 
-				// second triangle
-				ab = vecBottomRt - vecTopLt;
-				ac = vecTopRt		 - vecTopLt;
-				normal = glm::normalize(glm::cross(ab, ac));
+        indices.push_back(topLt);
+        indices.push_back(bottomLt);
+        indices.push_back(bottomRt);
 
-				indices.push_back(topLt);
-				indices.push_back(bottomRt);
-				indices.push_back(topRt);
+        vertices.at(topLt).Normal += normal;
+        vertices.at(bottomLt).Normal += normal;
+        vertices.at(bottomRt).Normal += normal;
 
-				vertices.at(topLt).Normal += normal;
-				vertices.at(bottomLt).Normal += normal;
-				vertices.at(bottomRt).Normal += normal;
-			}
-		}
+        // second triangle
+        ab = vecBottomRt - vecTopLt;
+        ac = vecTopRt - vecTopLt;
+        normal = glm::normalize(glm::cross(ab, ac));
 
-		// Normalize vertices normals
-		for (int i = 0; i < vertices.size(); i++) {
-			vertices.at(i).Normal = glm::normalize(vertices.at(i).Normal);
-		}
+        indices.push_back(topLt);
+        indices.push_back(bottomRt);
+        indices.push_back(topRt);
 
-		this->wasGenerated = true;
-		return Mesh(vertices, indices, textures);
-	}
+        vertices.at(topLt).Normal += normal;
+        vertices.at(bottomLt).Normal += normal;
+        vertices.at(bottomRt).Normal += normal;
+      }
+    }
 
-	float Terrain::getHeightAtCoord(int x, int y, int z) {
-		float xf = (float)x;
-		float yf = (float)y;
-		float zf = (float)z;
-		float maxed = (float)(1 << (octaves - 1));
-		float result = 0.0f;
+    // Normalize vertices normals
+    for (int i = 0; i < vertices.size(); i++) {
+      vertices.at(i).Normal = glm::normalize(vertices.at(i).Normal);
+    }
 
-		float amplitude = 1;
-		float frequency = .5;
+    this->wasGenerated = true;
+    return Mesh(vertices, indices, textures);
+  }
 
-		// wave generation by combination noises at different frequencies and amplitudes
-		for (int i = 0; i < octaves; i++) {
-			result += perlinNoise.getNoise(glm::vec3(xf, yf, zf) * frequency) * amplitude;
-			amplitude *= .5;
-			frequency *= 2;
-		}
+  float Terrain::getHeightAtCoord(int x, int y, int z) {
+    float xf = (float)x;
+    float yf = (float)y;
+    float zf = (float)z;
+    float maxed = (float)(1 << (octaves - 1));
+    float result = 0.0f;
 
-		return result;
-	}
+    float amplitude = 1;
+    float frequency = .5;
+
+    // wave generation by combination noises at different frequencies and amplitudes
+    for (int i = 0; i < octaves; i++) {
+      result += perlinNoise.getNoise(glm::vec3(xf, yf, zf) * frequency) * amplitude;
+      amplitude *= .5;
+      frequency *= 2;
+    }
+
+    return result;
+  }
 }
--- PGSceneFinalProject/main.cpp
+++ PGSceneFinalProject/main.cpp
@@ -24,7 +24,7 @@
 int glWindowWidth = 640;
 int glWindowHeight = 480;
 int retina_width, retina_height;
-GLFWwindow* glWindow = NULL;
+GLFWwindow *glWindow = NULL;
 
 glm::mat4 model;
 GLuint modelLoc;
@@ -55,264 +55,287 @@ gps::Terrain terrain;
 
 GLenum glCheckError_(const char *file, int line)
 {
-	GLenum errorCode;
-	while ((errorCode = glGetError()) != GL_NO_ERROR)
-	{
-		std::string error;
-		switch (errorCode)
-		{
-		case GL_INVALID_ENUM:                  error = "INVALID_ENUM"; break;
-		case GL_INVALID_VALUE:                 error = "INVALID_VALUE"; break;
-		case GL_INVALID_OPERATION:             error = "INVALID_OPERATION"; break;
-		case GL_STACK_OVERFLOW:                error = "STACK_OVERFLOW"; break;
-		case GL_STACK_UNDERFLOW:               error = "STACK_UNDERFLOW"; break;
-		case GL_OUT_OF_MEMORY:                 error = "OUT_OF_MEMORY"; break;
-		case GL_INVALID_FRAMEBUFFER_OPERATION: error = "INVALID_FRAMEBUFFER_OPERATION"; break;
-		}
-		std::cout << error << " | " << file << " (" << line << ")" << std::endl;
-	}
-	return errorCode;
+  GLenum errorCode;
+  while ((errorCode = glGetError()) != GL_NO_ERROR)
+  {
+    std::string error;
+    switch (errorCode)
+    {
+    case GL_INVALID_ENUM:
+      error = "INVALID_ENUM";
+      break;
+    case GL_INVALID_VALUE:
+      error = "INVALID_VALUE";
+      break;
+    case GL_INVALID_OPERATION:
+      error = "INVALID_OPERATION";
+      break;
+    case GL_STACK_OVERFLOW:
+      error = "STACK_OVERFLOW";
+      break;
+    case GL_STACK_UNDERFLOW:
+      error = "STACK_UNDERFLOW";
+      break;
+    case GL_OUT_OF_MEMORY:
+      error = "OUT_OF_MEMORY";
+      break;
+    case GL_INVALID_FRAMEBUFFER_OPERATION:
+      error = "INVALID_FRAMEBUFFER_OPERATION";
+      break;
+    }
+    std::cout << error << " | " << file << " (" << line << ")" << std::endl;
+  }
+  return errorCode;
 }
 
 #define glCheckError() glCheckError_(__FILE__, __LINE__)
 
-void windowResizeCallback(GLFWwindow* window, int width, int height)
+void windowResizeCallback(GLFWwindow *window, int width, int height)
 {
-	fprintf(stdout, "window resized to width: %d , and height: %d\n", width, height);
-	//TODO
-	//for RETINA display
-	glfwGetFramebufferSize(glWindow, &retina_width, &retina_height);
+  fprintf(stdout, "window resized to width: %d , and height: %d\n", width, height);
+  //TODO
+  //for RETINA display
+  glfwGetFramebufferSize(glWindow, &retina_width, &retina_height);
 
-	//set projection matrix
-	glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)retina_width / (float)retina_height, 0.1f, 1000.0f);
-	//send matrix data to shader
-	GLint projLoc = glGetUniformLocation(myCustomShader.shaderProgram, "projection");
-	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+  //set projection matrix
+  glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)retina_width / (float)retina_height, 0.1f, 1000.0f);
+  //send matrix data to shader
+  GLint projLoc = glGetUniformLocation(myCustomShader.shaderProgram, "projection");
+  glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
-	//set Viewport transform
-	glViewport(0, 0, retina_width, retina_height);
+  //set Viewport transform
+  glViewport(0, 0, retina_width, retina_height);
 }
 
-void keyboardCallback(GLFWwindow* window, int key, int scancode, int action, int mode)
+void keyboardCallback(GLFWwindow *window, int key, int scancode, int action, int mode)
 {
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, GL_TRUE);
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GL_TRUE);
 
-	if (key >= 0 && key < 1024)
-	{
-		if (action == GLFW_PRESS)
-			pressedKeys[key] = true;
-		else if (action == GLFW_RELEASE)
-			pressedKeys[key] = false;
-	}
+  if (key >= 0 && key < 1024)
+  {
+    if (action == GLFW_PRESS)
+      pressedKeys[key] = true;
+    else if (action == GLFW_RELEASE)
+      pressedKeys[key] = false;
+  }
 }
 
 void processMovement()
 {
-	if (pressedKeys[GLFW_KEY_Q]) {
-		angle += 0.1f;
-		if (angle > 360.0f)
-			angle -= 360.0f;
-	}
+  if (pressedKeys[GLFW_KEY_Q])
+  {
+    angle += 0.1f;
+    if (angle > 360.0f)
+      angle -= 360.0f;
+  }
 
-	if (pressedKeys[GLFW_KEY_E]) {
-		angle -= 0.1f;
-		if (angle < 0.0f)
-			angle += 360.0f;
-	}
+  if (pressedKeys[GLFW_KEY_E])
+  {
+    angle -= 0.1f;
+    if (angle < 0.0f)
+      angle += 360.0f;
+  }
 
-	if (pressedKeys[GLFW_KEY_W]) {
-		myCamera.move(gps::MOVE_FORWARD, cameraSpeed);
-	}
+  if (pressedKeys[GLFW_KEY_W])
+  {
+    myCamera.move(gps::MOVE_FORWARD, cameraSpeed);
+  }
 
-	if (pressedKeys[GLFW_KEY_S]) {
-		myCamera.move(gps::MOVE_BACKWARD, cameraSpeed);
-	}
+  if (pressedKeys[GLFW_KEY_S])
+  {
+    myCamera.move(gps::MOVE_BACKWARD, cameraSpeed);
+  }
 
-	if (pressedKeys[GLFW_KEY_A]) {
-		myCamera.move(gps::MOVE_LEFT, cameraSpeed);
-	}
+  if (pressedKeys[GLFW_KEY_A])
+  {
+    myCamera.move(gps::MOVE_LEFT, cameraSpeed);
+  }
 
-	if (pressedKeys[GLFW_KEY_D]) {
-		myCamera.move(gps::MOVE_RIGHT, cameraSpeed);
-	}
+  if (pressedKeys[GLFW_KEY_D])
+  {
+    myCamera.move(gps::MOVE_RIGHT, cameraSpeed);
+  }
 }
 
-void mouseCallback(GLFWwindow* window, double xpos, double ypos) {
-	static bool firstMouseCallback = true;
-	static double xlast, ylast;
-	static const float cameraRotationSensitivity = 5e-1;
-	static double yaw = 0, pitch = 0;
+void mouseCallback(GLFWwindow *window, double xpos, double ypos)
+{
+  static bool firstMouseCallback = true;
+  static double xlast, ylast;
+  static const float cameraRotationSensitivity = 5e-1;
+  static double yaw = 0, pitch = 0;
 
+  if (firstMouseCallback)
+  {
+    firstMouseCallback = false;
+    xlast = xpos;
+    ylast = ypos;
+    return;
+  }
 
-	if (firstMouseCallback) {
-		firstMouseCallback = false;
-		xlast = xpos;
-		ylast = ypos;
-		return;
-	}
+  float offsetx = (xpos - xlast) * cameraRotationSensitivity;
+  float offsety = (ypos - ylast) * cameraRotationSensitivity;
 
-	float offsetx = (xpos - xlast) * cameraRotationSensitivity;
-	float offsety = (ypos - ylast) * cameraRotationSensitivity;
+  xlast = xpos, ylast = ypos;
 
-	xlast = xpos, ylast = ypos;
+  yaw += offsetx;
+  pitch += offsety;
 
-	yaw += offsetx;
-	pitch += offsety;
-
-	//yaw		= coerce(yaw,		-89.0f, 89.0f);
-	pitch = gps::coerce(pitch, -89.0f, 89.0f);
-	myCamera.rotate(-pitch, yaw);
+  //yaw		= coerce(yaw,		-89.0f, 89.0f);
+  pitch = gps::coerce(pitch, -89.0f, 89.0f);
+  myCamera.rotate(-pitch, yaw);
 }
 
 bool initOpenGLWindow()
 {
-	if (!glfwInit()) {
-		fprintf(stderr, "ERROR: could not start GLFW3\n");
-		return false;
-	}
+  if (!glfwInit())
+  {
+    fprintf(stderr, "ERROR: could not start GLFW3\n");
+    return false;
+  }
 
-	// for Mac OS X
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  // for Mac OS X
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	glWindow = glfwCreateWindow(glWindowWidth, glWindowHeight, "Marcus Pop project", NULL, NULL);
-	if (!glWindow) {
-		fprintf(stderr, "ERROR: could not open window with GLFW3\n");
-		glfwTerminate();
-		return false;
-	}
+  glWindow = glfwCreateWindow(glWindowWidth, glWindowHeight, "Marcus Pop project", NULL, NULL);
+  if (!glWindow)
+  {
+    fprintf(stderr, "ERROR: could not open window with GLFW3\n");
+    glfwTerminate();
+    return false;
+  }
 
-	glfwSetWindowSizeCallback(glWindow, windowResizeCallback);
-	glfwMakeContextCurrent(glWindow);
-	glfwWindowHint(GLFW_SAMPLES, 4);
+  glfwSetWindowSizeCallback(glWindow, windowResizeCallback);
+  glfwMakeContextCurrent(glWindow);
+  glfwWindowHint(GLFW_SAMPLES, 4);
 
-	// start GLEW extension handler
-	glewExperimental = GL_TRUE;
-	glewInit();
+  // start GLEW extension handler
+  glewExperimental = GL_TRUE;
+  glewInit();
 
-	// get version info
-	const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
-	const GLubyte* version = glGetString(GL_VERSION); // version as a string
+  // get version info
+  const GLubyte *renderer = glGetString(GL_RENDERER); // get renderer string
+  const GLubyte *version = glGetString(GL_VERSION);   // version as a string
 
-	printf("Renderer: %s\n", renderer);
-	printf("OpenGL version supported %s\n", version);
+  printf("Renderer: %s\n", renderer);
+  printf("OpenGL version supported %s\n", version);
 
-	// for RETINA display
-	glfwGetFramebufferSize(glWindow, &retina_width, &retina_height);
-	glfwSetKeyCallback(glWindow, keyboardCallback);
+  // for RETINA display
+  glfwGetFramebufferSize(glWindow, &retina_width, &retina_height);
+  glfwSetKeyCallback(glWindow, keyboardCallback);
 
-	// disable cursor and bind callback for mouse events
-	glfwSetCursorPosCallback(glWindow, mouseCallback);
-	glfwSetInputMode(glWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+  // disable cursor and bind callback for mouse events
+  glfwSetCursorPosCallback(glWindow, mouseCallback);
+  glfwSetInputMode(glWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-	return true;
+  return true;
 }
 
 void initOpenGLState()
 {
-	glClearColor(0.3, 0.3, 0.3, 1.0);
-	glViewport(0, 0, retina_width, retina_height);
+  glClearColor(0.3, 0.3, 0.3, 1.0);
+  glViewport(0, 0, retina_width, retina_height);
 
-	glEnable(GL_DEPTH_TEST); // enable depth-testing
-	glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
-	glEnable(GL_CULL_FACE); // cull face
-	glCullFace(GL_BACK); // cull back face
-	glFrontFace(GL_CCW); // GL_CCW for counter clock-wise
+  glEnable(GL_DEPTH_TEST); // enable depth-testing
+  glDepthFunc(GL_LESS);    // depth-testing interprets a smaller value as "closer"
+  glEnable(GL_CULL_FACE);  // cull face
+  glCullFace(GL_BACK);     // cull back face
+  glFrontFace(GL_CCW);     // GL_CCW for counter clock-wise
 
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 }
 
 void initModels()
 {
-	//myModel = gps::Model3D("objects/nanosuit/nanosuit.obj", "objects/nanosuit/");
-	terrain = gps::Terrain(64);
+  //myModel = gps::Model3D("objects/nanosuit/nanosuit.obj", "objects/nanosuit/");
+  terrain = gps::Terrain(64);
 }
 
 void initShaders()
 {
-	terrainShader.loadShader("shaders/terrainShader.vert", "shaders/terrainShader.frag");
-	terrainShader.useShaderProgram();
-	
-	myCustomShader.loadShader("shaders/shaderStart.vert", "shaders/shaderStart.frag");
-	//myCustomShader.useShaderProgram();
-	
+  terrainShader.loadShader("shaders/terrainShader.vert", "shaders/terrainShader.frag");
+  terrainShader.useShaderProgram();
+
+  myCustomShader.loadShader("shaders/shaderStart.vert", "shaders/shaderStart.frag");
+  //myCustomShader.useShaderProgram();
 }
 
 void initUniforms()
 {
-	model = glm::mat4(1.0f);
-	modelLoc = glGetUniformLocation(terrainShader.shaderProgram, "model");
-	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+  model = glm::mat4(1.0f);
+  modelLoc = glGetUniformLocation(terrainShader.shaderProgram, "model");
+  glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
-	view = myCamera.getViewMatrix();
-	viewLoc = glGetUniformLocation(terrainShader.shaderProgram, "view");
-	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-	
-	normalMatrix = glm::mat3(glm::inverseTranspose(view*model));
-	normalMatrixLoc = glGetUniformLocation(terrainShader.shaderProgram, "normalMatrix");
-	glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, glm::value_ptr(normalMatrix));
-	
-	projection = glm::perspective(glm::radians(45.0f), (float)retina_width / (float)retina_height, 0.1f, 1000.0f);
-	projectionLoc = glGetUniformLocation(terrainShader.shaderProgram, "projection");
-	glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+  view = myCamera.getViewMatrix();
+  viewLoc = glGetUniformLocation(terrainShader.shaderProgram, "view");
+  glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 
-	//set the light direction (direction towards the light)
-	lightDir = glm::vec3(0.0f, 1.0f, 1.0f);
-	lightDirLoc = glGetUniformLocation(terrainShader.shaderProgram, "lightDir");
-	glUniform3fv(lightDirLoc, 1, glm::value_ptr(lightDir));
+  normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+  normalMatrixLoc = glGetUniformLocation(terrainShader.shaderProgram, "normalMatrix");
+  glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, glm::value_ptr(normalMatrix));
 
-	//set light color
-	lightColor = glm::vec3(1.0f, 1.0f, 1.0f); //white light
-	lightColorLoc = glGetUniformLocation(terrainShader.shaderProgram, "lightColor");
-	glUniform3fv(lightColorLoc, 1, glm::value_ptr(lightColor));
+  projection = glm::perspective(glm::radians(45.0f), (float)retina_width / (float)retina_height, 0.1f, 1000.0f);
+  projectionLoc = glGetUniformLocation(terrainShader.shaderProgram, "projection");
+  glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+  //set the light direction (direction towards the light)
+  lightDir = glm::vec3(0.0f, 1.0f, 1.0f);
+  lightDirLoc = glGetUniformLocation(terrainShader.shaderProgram, "lightDir");
+  glUniform3fv(lightDirLoc, 1, glm::value_ptr(lightDir));
+
+  //set light color
+  lightColor = glm::vec3(1.0f, 1.0f, 1.0f); //white light
+  lightColorLoc = glGetUniformLocation(terrainShader.shaderProgram, "lightColor");
+  glUniform3fv(lightColorLoc, 1, glm::value_ptr(lightColor));
 }
 
 void renderScene()
 {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	processMovement();
+  processMovement();
 
-	//myCustomShader.useShaderProgram();
+  //myCustomShader.useShaderProgram();
 
-	view = myCamera.getViewMatrix();
-	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+  view = myCamera.getViewMatrix();
+  glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 
-	model = glm::mat4(1.0f);
-	model = glm::rotate(model, glm::radians(angle), glm::vec3(0, 1, 0));
-	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+  model = glm::mat4(1.0f);
+  model = glm::rotate(model, glm::radians(angle), glm::vec3(0, 1, 0));
+  glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
-	normalMatrix = glm::mat3(glm::inverseTranspose(view*model));
-	glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, glm::value_ptr(normalMatrix));
-	
-	//myModel.Draw(myCustomShader);
+  normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+  glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, glm::value_ptr(normalMatrix));
 
-	//terrainShader.useShaderProgram();
-	terrain.Draw(terrainShader);
+  //myModel.Draw(myCustomShader);
+
+  //terrainShader.useShaderProgram();
+  terrain.Draw(terrainShader);
 }
 
+int main(int argc, const char *argv[])
+{
 
-int main(int argc, const char * argv[]) {
+  initOpenGLWindow();
+  initOpenGLState();
+  initModels();
+  initShaders();
+  initUniforms();
 
-	initOpenGLWindow();
-	initOpenGLState();
-	initModels();
-	initShaders();
-	initUniforms();	
-	
-	while (!glfwWindowShouldClose(glWindow)) {
-		renderScene();
+  while (!glfwWindowShouldClose(glWindow))
+  {
+    renderScene();
 
-		glfwPollEvents();
-		glfwSwapBuffers(glWindow);
-	}
+    glfwPollEvents();
+    glfwSwapBuffers(glWindow);
+  }
 
-	//close GL context and any other GLFW resources
-	glfwTerminate();
+  //close GL context and any other GLFW resources
+  glfwTerminate();
 
-	return 0;
+  return 0;
 }
